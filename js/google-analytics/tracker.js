5f23755e8f02da05e7686011e1c216f66e7e56c7
Joao Silva <Joao.Silva@cardinalpath.com>
2019-06-06 10:37:48 -0700

    tracker.js

--- js/google-analytics/tracker.js
+++ js/google-analytics/tracker.js
@@ -1,34 +1,27 @@
 $(function () {
-    
-    console.log('jQuery loaded');
-    
-    $('.card').on('click', function() {
-        console.log('Hello there!');
-    })
-    
-    // console.log('Google analytics tracker loaded');
-    //
-    // //User clicks on a product
-    // $('.card').on('click', function () {
-    //
-    //     const product = {
-    //         name: $(this).find('.dark-grey-text').text().replace('NEW', '').replace('bestseller', '').trim(),
-    //         price: $(this).find('.font-weight-bold.blue-text strong').text().replace('$', '')
-    //     };
-    //
-    //     console.log(product);
-    //
-    //     console.log(`User clicked on a ${product.name}`);
-    //
-    //     console.log('Sending event to GTM');
-    //
-    //     dataLayer.push({
-    //         'event': 'products',
-    //         'product_name': product.name,
-    //         'product_price': product.price
-    //     })
-    //
-    //
-    // });
+
+
+    $('.card').on('mousedown', function () {
+
+        console.log('Google analytics tracker loaded');
+
+        const product = {
+            name: $(this).find('.dark-grey-text').text().replace('NEW', '').replace('bestseller', '').trim(),
+            price: $(this).find('.font-weight-bold.blue-text strong').text().replace('$', '')
+        };
+
+
+        console.log(`User clicked on a ${product.name}`);
+        console.log('Sending event to GTM');
+
+        dataLayer.push({
+            'event': 'product_click',
+            'product_name': product.name,
+            'product_price': product.price
+        });
+
+
+
+    });
 
 });
